5b6de93e5ccabcbf5ebdd42fb5a6a8f72b383e67
hatwell <caroline.hatwell@gmail.com>
2017-05-02 11:03:04 +0100

    set up db

--- models/job.rb
+++ models/job.rb
@@ -10,4 +10,30 @@ class Job
     @stage_of_process = params['stage_of_process']
     @application_sent = param['application_sent']
   end
+
+  def save
+    sql = "INSERT INTO jobs(position, company, job_offer, stage_of_process, application_sent) VALUES ('#{@position}', '#{@company}', #{@job_offer}, '#{@stage_of_process}', #{@application_sent}) RETURNING *"
+    results = SqlRunner.run(sql)
+    @id = results.first['id'].to_i
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def find_by_id(id)
+  end
+
+  def self.all
+    sql = "SELECT * FROM jobs"
+    Job.map_items(sql)
+  end
+
+  def self.map_items
+    jobs = SqlRunner.run(sql)
+    result = jobs.map {|job| Job.new(job)}
+    return result
+  end
 end
